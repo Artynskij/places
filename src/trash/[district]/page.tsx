faf719839d8958f4282e881d24f514d69679c8e3
Artynskij <85955374+Artynskij@users.noreply.github.com>
2025-04-10 14:26:35 +0300

    test: trash to comment

--- src/trash/[district]/page.tsx
+++ src/trash/[district]/page.tsx
@@ -1,41 +1,41 @@
-import CountryScreen from "@/screens/LocationScreen/CountryScreen";
-import { IPageProps } from "@/lib/models/IType";
+// import CountryScreen from "@/screens/LocationScreen/CountryScreen";
+// import { IPageProps } from "@/lib/models/IType";
 
-import { notFound } from "next/navigation";
-import { EstablishmentService } from "@/lib/Api/establishment/establishment.service";
+// import { notFound } from "next/navigation";
+// import { EstablishmentService } from "@/lib/Api/establishment/establishment.service";
 
-export async function generateMetadata({
-    params,
-}: {
-    params: { district: string };
-}) {
-    return {
-        title: `${process.env.BASE_NAME} | ${params.district}`,
-    };
-}
+// export async function generateMetadata({
+//     params,
+// }: {
+//     params: { district: string };
+// }) {
+//     return {
+//         title: `${process.env.BASE_NAME} | ${params.district}`,
+//     };
+// }
 
-interface IProps extends IPageProps {
-    params: IPageProps["params"] & {
-        country: string;
-        district: string;
-    };
-}
-export default async function DistrictPage({ params, searchParams }: IProps) {
-    const api = new EstablishmentService();
+// interface IProps extends IPageProps {
+//     params: IPageProps["params"] & {
+//         country: string;
+//         district: string;
+//     };
+// }
+// export default async function DistrictPage({ params, searchParams }: IProps) {
+//     const api = new EstablishmentService();
 
-    const data = await api.getEstablishmentByPagination({
-        pagination: { page: 1, pageSize: 5 },
-        lang: params.locale,
-    });
-    if (!data) notFound();
-    return (
-        <>
-            <CountryScreen
-                typePage="district"
-                params={params}
-                searchParams={searchParams}
-                dataEstablishment={data}
-            />
-        </>
-    );
-}
+//     const data = await api.getEstablishmentByPagination({
+//         pagination: { page: 1, pageSize: 5 },
+//         lang: params.locale,
+//     });
+//     if (!data) notFound();
+//     return (
+//         <>
+//             <CountryScreen
+//                 typePage="district"
+//                 params={params}
+//                 searchParams={searchParams}
+//                 dataEstablishment={data}
+//             />
+//         </>
+//     );
+// }
